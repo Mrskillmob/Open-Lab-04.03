779b41fc7e90a50bee48e05cfac68bd11d405229
Martin Kubica <martinko29.km@gmail.com>
2020-10-25 18:10:46 +0100

    +

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._03
 {
@@ -6,7 +7,28 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length < 2)
+            {
+                return original;
+            }
+            
+            char[] unoriginal = new char[original.Length-1];
+            for (int i = 0; i < original.Length-1; i++)
+            {
+                unoriginal[i] = original[i];
+            }
+            Array.Reverse(unoriginal);
+            char[] rg3 = new char[unoriginal.Length - 1];
+            for (int i = 0; i < unoriginal.Length-1; i++)
+            {
+                rg3[i] = unoriginal[i];
+            }
+            Array.Reverse(rg3);
+            string a = String.Join("", rg3);
+            return a;
+            
+
+
         }
     }
 }
